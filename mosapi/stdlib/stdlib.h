97327b43422554cf451ce5948005dabc4bb3b5ad
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-05-07 19:10:42 +0700

    mosapi root folder has been created

---
+++ mosapi/stdlib/stdlib.h
@@ -0,0 +1,27 @@
+#ifndef MOUNTAIN__OS__STDLIB__H__
+#define MOUNTAIN__OS__STDLIB__H__
+    // CONSTANTS
+    #define NULL 0
+
+    // TYPES
+    typedef unsigned int size_t;
+    typedef struct {
+        int quot, rem;
+    } div_t;
+    typedef struct {
+        long int quot, rem;
+    } ldiv_t;    
+
+
+    // FUNCTIONS
+    int atoi(const char *str);
+    long atol(const char *str);
+    long long atoll(const char *str);
+    //double atof(const char *str);
+
+    int abs(int arg);
+    long labs(long arg);
+    long long llabs(long long arg);
+    double fabs(double arg);
+
+#endif /* MOUNTAIN__OS__STDLIB__H__ */
