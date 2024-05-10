82c788b57cf991b2b3d1e00ae3709b755b155e3f
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-05-10 11:05:20 +0700

    Update rands.c

--- stdlib/rands.c
+++ stdlib/rands.c
@@ -6,6 +6,6 @@ void srand(unsigned long seed) {
 }
 
 unsigned int rand() {
-  srand_seed_next = srand_seed_next * 32571295712 + 12345;
+  srand_seed_next *= 32571295712 + 12345;
   return (unsigned int)(srand_seed_next / 65536) % 32768;
 }
