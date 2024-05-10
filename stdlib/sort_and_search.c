500f09cbabf9307f9bd210f808174be3c90cc27f
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-05-10 16:59:52 +0700

    Update sort_and_search.c

--- stdlib/sort_and_search.c
+++ stdlib/sort_and_search.c
@@ -1,28 +1,49 @@
 #include "stdlib.h"
 #include <stdio.h>
 
-#define TYPE_BYTE 1
-#define TYPE_INT  2
-#define TYPE_LONG 4
 
-void qsort(
-        void *arr_ptr,
-        size_t arr_len,
-        size_t element_size,
-        int (*comparator)(const void*, const void*)
-    ) {
-        size_t i, max_depth = 0;
-        int swap_type;
 
-        for (i = arr_len; i > 0; i >>= 1) max_depth++;
-        max_depth *= 2;
+//--------------------------------------------------------------QUICK SORT--------------------------------------------------------------\\
+static void exchange(char* first_elem, char* second_elem, unsigned size) {
+  while (size--) {
+    int temp = *first_elem;
+    *first_elem++ = *second_elem;
+    *second_elem++ = temp;
+  }
+}
 
-        switch (element_size) {
-            case TYPE_BYTE: swap_type = TYPE_BYTE; break;
-            case TYPE_INT:  swap_type = TYPE_INT;  break;
-            case TYPE_LONG: swap_type = TYPE_LONG; break;
-        }
+void qsort(void* arr, unsigned element_count, unsigned element_size, int (*comparator)(void*, void*)) {
+  unsigned heap_size = 0;
+
+  // возможен stack overflow
+  while (element_count--) {
+    unsigned i = heap_size;
+    unsigned parent = (i - 1) >> 1;
+    while (i && comparator((char*)arr + parent * element_size, (char*)arr + i * element_size) < 0) {
+      exchange((char*)arr + parent * element_size, (char*)arr + i * element_size, element_size);
+      i = parent;
+      parent = (parent - 1) >> 1;
     }
+    heap_size++;
+  }
+
+  while (heap_size--) {
+    unsigned i = 0;
+    exchange((char*)arr, (char*)arr + heap_size * element_size, element_size);
+    while (i < (heap_size >> 1)) {
+      unsigned left = (i << 1) + 1;
+      unsigned right = left + 1;
+      unsigned candidate = left;
+      
+      if (right < heap_size && comparator((char*)arr + right * element_size, (char*)arr + left * element_size) > 0) candidate = right;
+      if (comparator((char*)arr + i * element_size, (char*)arr + candidate * element_size) >= 0) break;
+      exchange((char*)arr + i * element_size, (char*)arr + candidate * element_size, element_size);
+      i = candidate;
+    }
+  }
+}
+
+//--------------------------------------------------------------BINARY SEARCH--------------------------------------------------------------\\
 
 void* bsearch(void* key, void* arr, unsigned element_count, unsigned element_size, int (*comparator)(void*, void*)) {
   while (ecount) {
