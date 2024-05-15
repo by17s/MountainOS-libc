e98cba37a4d79c1d85003bf5a00f021ded6a3c16
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-05-15 21:29:24 +0700

    Update string.h

--- string/string.h
+++ string/string.h
@@ -3,4 +3,10 @@
 
 size_t strlen(const char *str);
 
+char*  strcpy(char *to_here, const char *from_here);
+char*  strncpy(char *to_here, const char *from_here, size_t len);
+
+int strcmp(const char *str1, const char *str2);
+int strncmp(const char *str1, const char *str2, size_t len);
+
 #endif /*MOUNTAIN__OS__STRING__H__*/
