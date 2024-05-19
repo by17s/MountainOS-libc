bde093ac1e2cb4f718d369e664db0ccd67d080c5
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-05-19 20:08:47 +0700

    Update string.h

--- string/string.h
+++ string/string.h
@@ -1,12 +1,15 @@
 #ifndef MOUNTAIN__OS__STRING__H__
 #define MOUNTAIN__OS__STRING__H__
 
-size_t strlen(const char *str);
+size_t strlen(const char* str);
 
-char*  strcpy(char *to_here, const char *from_here);
-char*  strncpy(char *to_here, const char *from_here, size_t len);
+char*  strcpy(char* to_here, const char* from_here);
+char*  strncpy(char* to_here, const char* from_here, size_t len);
 
-int strcmp(const char *str1, const char *str2);
-int strncmp(const char *str1, const char *str2, size_t len);
+int strcmp(const char* str1, const char* str2);
+int strncmp(const char* str1, const char* str2, size_t len);
+
+char* strchr(const char* s, int c);
+char* strrchr(const char* s, int c);
 
 #endif /*MOUNTAIN__OS__STRING__H__*/
