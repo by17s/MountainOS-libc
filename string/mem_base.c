a4f26003b3effd7d9e12116319ef1ed67d746ed2
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-05-19 20:43:02 +0700

    Create mem_base.c

---
+++ string/mem_base.c
@@ -0,0 +1,11 @@
+#include "../stdlib/stdlib.h"
+#include "string.h"
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    if (dest == src) return;
+
+    int* dest_i = dest;
+    int* src_i  = src;
+
+    for (int i = 0; i < n; i++) dest_i[i] = src_i[i];
+}
