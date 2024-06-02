6fd387af164287975eb557c446a5aed04448e698
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2024-06-02 22:41:42 +0700

    Update string.h

--- string/string.h
+++ string/string.h
@@ -3,11 +3,13 @@
 
 size_t strlen(const char* str);
 
-char*  strcpy(char* to_here, const char* from_here);
-char*  strncpy(char* to_here, const char* from_here, size_t len);
+char* strcpy(char* to_here, const char* from_here);
+char* strncpy(char* to_here, const char* from_here, size_t len);
 
 int strcmp(const char* str1, const char* str2);
 int strncmp(const char* str1, const char* str2, size_t len);
+
+void strcat(char* dest, const char* src);
 
 char* strchr(const char* s, int c);
 char* strrchr(const char* s, int c);
